c2e1f5a0773e2ebcfb334a044b52ff68199211b9
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2020-04-28 23:49:23 +0530

    statically fixing UI, API defined
    Api key defined and testing the async-await function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 import Weather from './component/weather'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import 'weather-icons/css/weather-icons.css'
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
+
+
+const API_key = '6e451c1baa67113a3ddddfc816a006b3'
+class App extends Component { 
+  constructor(props) {
+    super(props)
+    this.state = {}
+    this.getWeather('api.openweathermap.org/data/2.5/weather?q=London,uk')
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`)
+    const response = await api_call.json()
+    console.log(response)
+  }
+  render() {
+    return (
+      <div className="container text-center">
+        <Weather />
+      </div>
+    )
+  }
 }
 
-export default App;
+  export default App;
--- src/component/weather.js
+++ src/component/weather.js
@@ -4,11 +4,13 @@ const weather = () => {
     return (
         <div className="container">
             <div className="cards">
-                <h1>App</h1>
+                <h1>London</h1>
                 <h5 className="py-4">
                     <i className="wi wi-day-sunny display-1"></i>
                 </h5>
                 <h1 className="py-2">25&deg;</h1>
+                {minmaxTemp(24,19)}
+                <h4 className="py-2">Slow Rain</h4>
             </div>
         </div>
     );
